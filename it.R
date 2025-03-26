436991094d062f93e7b887660c46f4367bf4dffb
korikedina <113836770+korikedina@users.noreply.github.com>
2025-03-26 10:51:23 +0100

    independence testing

---
+++ it.R
@@ -0,0 +1,19 @@
+it <- function(m){
+	ki_=rowSums(m)
+	k_j=colSums(m)
+	N=sum(m)
+	print(N)
+	r=nrow(m)
+	s=ncol(m)
+	my_chi_sq=0
+	for(i in 1:r){
+		for(j in 1:s){
+			my_chi_sq=my_chi_sq+(m[i, j]-ki_[i]*k_j[j]/N)^2/(ki_[i]*k_j[j]/N)
+		}
+	}
+
+	if(my_chi_sq<=qchisq(p=.05, df=(r-1)*(s-1))){
+		return("ACCEPT")
+	}
+	return("DECLINE")
+}
